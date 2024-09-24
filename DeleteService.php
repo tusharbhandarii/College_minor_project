54ba051605319b44dc4a27f73807c621c63a12ff
tusharbhandarii <15tusharx2@gmail.com>
2024-09-24 13:05:36 +0530

    delete data feature in service module

---
+++ DeleteService.php
@@ -0,0 +1,12 @@
+<?php
+    $q = $_GET['q'];
+
+    $con = mysqli_connect('localhost','root','','demoproject');
+    $delequery = "DELETE FROM service WHERE slno='$q'";
+    
+    if(mysqli_query($con,$delequery)){
+        echo "<script> alert('Data Deleted !!');window.location.href ='AddService.php'; </script>";
+    }else{
+        echo "<script> alert('Data not Deleted !!') window.location.href ='AddService.php' </script>";
+    }
+?>
